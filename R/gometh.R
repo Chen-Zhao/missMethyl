131903399e213274fef527175fed74b974ca119f
Chen-Zhao <zhaochen.tcm@gmail.com>
2023-03-12 00:11:46 +0100

    Update gometh.R
    patch kegg

--- R/gometh.R
+++ R/gometh.R
@@ -266,7 +266,7 @@ gometh <- function(sig.cpg, all.cpg=NULL, collection=c("GO","KEGG"),
   ID.ID <- paste(GeneID.PathID[, 1], GeneID.PathID[, 2], sep = ".")
   d <- !duplicated(ID.ID)
   GeneID.PathID <- GeneID.PathID[d, ]
-  PathID.PathName <- limma::getKEGGPathwayNames(species.KEGG = "Hsa", 
+  PathID.PathName <- limma::getKEGGPathwayNames(species.KEGG = "hsa", 
                                          remove.qualifier = TRUE)
   GeneID.PathID <- merge(GeneID.PathID, PathID.PathName, by="PathwayID")
   kegg <- tapply(GeneID.PathID$GeneID, GeneID.PathID$PathwayID, list)
